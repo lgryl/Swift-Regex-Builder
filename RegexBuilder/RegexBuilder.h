5bdf3880524bca6578372fb652092df9d57ad8b7
Łukasz Gryl <lukasz.gryl@gmail.com>
2020-11-06 20:03:03 +0100

    Initial commit

---
+++ RegexBuilder/RegexBuilder.h
@@ -0,0 +1,13 @@
+// Created 06/11/2020
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RegexBuilder.
+FOUNDATION_EXPORT double RegexBuilderVersionNumber;
+
+//! Project version string for RegexBuilder.
+FOUNDATION_EXPORT const unsigned char RegexBuilderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RegexBuilder/PublicHeader.h>
+
+
